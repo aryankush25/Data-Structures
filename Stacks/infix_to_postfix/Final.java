2d6f4cae0cf461c3bad5f0abf29ba32169036b54
Aryan Agarwal <aryankush025@gmail.com>
2018-10-19 14:25:07 +0530

    commit

--- Stacks/infix_to_postfix/Final.java
+++ Stacks/infix_to_postfix/Final.java
@@ -1,8 +1,33 @@
-import java.util.*;
-
+import java.lang.*;
 class Final
 {
+    public static boolean isChar(char s)
+    {
+        if((s >= 65 && s <= 90) || (s >= 97 && s <= 122))
+            return true;
+        return false;
+    }
     public static void main(String[] args) {
-        Stack<Integer> s = new ArrayStack<Integer>();
+        Stack<Character> s = new ArrayStack<Character>();
+
+        String infix = "a+b*c+(d/e)+f", postfix = "";
+        for(int i = 0; i < infix.length(); i++)
+        {
+            if(isChar(infix.charAt(i)))
+            {
+                postfix = postfix + infix.charAt(i);
+            }
+            else
+            {
+                s.push(infix.charAt(i));
+            }
+        }
+
+        while(!s.isEmpty())
+        {
+            postfix = postfix + s.pop();
+        }
+        
+        System.out.println(postfix);
     }
 }
