a7f884894cca5980a16153509f7b8d56eac8fdda
Aryan Agarwal <aryankush025@gmail.com>
2018-12-07 10:17:04 +0530

    Checking a tree is a BST or not

--- Trees/BinarySearchTree/CheckBinaryTreeIsBST/checkBST.cpp
+++ Trees/BinarySearchTree/CheckBinaryTreeIsBST/checkBST.cpp
@@ -30,6 +30,64 @@ void printInOrder(Node *root)
     printInOrder(root->right);
 }
 
+char minimum(Node *root)
+{
+    if (root->left == NULL) {
+        return root->value;
+    }
+    return minimum(root->left);
+}
+
+char maximum(Node *root)
+{
+    if (root->right == NULL) {
+        return root->value;
+    }
+    return minimum(root->right);
+}
+
+bool isBST(Node *root)
+{
+    if (root == NULL) {
+        return true;
+    }
+    else if (root->left == NULL && root->right == NULL) {
+        return true;
+    }
+    else if (root->left == NULL) {
+        if (minimum(root->right) > root->value) {
+            return isBST(root->right);
+        } else {
+            return false;
+        }
+    }
+    else if (root->right == NULL) {
+        if (maximum(root->left) <= root->value) {
+            return isBST(root->left);
+        } else {
+            return false;
+        }
+    }
+    if (minimum(root->right) > root->value && maximum(root->left) <= root->value) {
+        return (isBST(root->left) && isBST(root->right));
+    } else {
+        return false;
+    }
+}
+
+bool isBSTinRange(Node* root, char minVal, char maxVal)
+{
+    if (root == NULL) {
+        return true;
+    }
+
+    if (root->value >= minVal && root->value < maxVal) {
+        return (isBSTinRange(root->left, minVal, root->value) && isBSTinRange(root->right, root->value, maxVal));
+    } else {
+        return false;
+    }
+}
+
 int main()
 {
     Node *root = newNode('F');
@@ -42,9 +100,25 @@ int main()
     root->left->left->left = newNode('A');
     root->left->left->right = newNode('C');
     root->right->left->right = newNode('I');
-    root->right->left->right->left = newNode('H');
+    root->right->left->right->left = newNode('H'); // Change 'A' to 'H' to make it BST
 
-    printf("\nInorder - ");
+    // isBST() Function is More Complex and Time Taking
+
+    if (isBST(root)) {
+        cout<< "Is a BST\n";
+    } else {
+        cout<< "Is Not a BST\n";
+    }
+
+    // isBSTinRange() Function is more Efficient
+
+    if (isBSTinRange(root, 0, 122)) {
+        cout<< "Is a BST\n";
+    } else {
+        cout<< "Is Not a BST\n";
+    }
+
+    printf("Inorder - ");
     printInOrder(root);
 
     return 0;
